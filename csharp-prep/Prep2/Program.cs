1bdf8200ed46b180dcbb9d442e9903407b7a057b
Calan Whiting <calanwhiting@Cals-Mac.local>
2023-04-20 20:11:45 -0600

    Completed the assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int grade;
+        string letter = "";
+
+        Console.Write("What is your grade percentage? ");
+        string sgrade = Console.ReadLine();
+
+        grade = Convert.ToInt32(sgrade);
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = "D";
+        }
+        else if (grade >= 0 && grade < 60)
+        {
+            letter = "F";
+        }
+        else
+        {
+            Console.WriteLine("Try entering it again.");
+        }
+
+        Console.WriteLine($"Your grade for the class is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congradulations, you passed!");
+        }
+        else if (grade < 70 && grade is >= 0)
+        {
+            Console.WriteLine("Its ok, you can always try again!");
+        }
+        else
+        {
+            Console.WriteLine();
+        }
     }
 }

9d3af194579d078fd638afc0cf82b697f06046b0
whitingcal <whitingcal01@byui.edu>
2023-04-26 19:00:55 -0600

    completed all core requirements for Prep 1, 2, and 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,47 @@
 using System;
 
-class Program
+namespace Prep3
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        static void Main(string[] args)
+        {
+            string stringResponce = "a";
+            int responce = 0;
+
+            // Console.Write("What is the magic number? ");
+            // string stringMagicNum = Console.ReadLine();
+
+            //int magicNum = Convert.ToInt32(stringMagicNum);
+
+            Random randomGenerator = new Random();
+            int magicNum = randomGenerator.Next(1, 11);
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                stringResponce = Console.ReadLine();
+
+                responce = Convert.ToInt32(stringResponce);
+
+                if (responce < magicNum)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (responce > magicNum)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (responce == magicNum)
+                {
+                    Console.WriteLine("Thats it!");
+                }
+                else
+                {
+                    Console.WriteLine("How'd you do that?");
+                }
+
+            } while (responce != magicNum);
+        }
+    } 
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int responce = 1;
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (responce != 0)
+        {
+            Console.Write("Add a number: ");
+            string stringResponce = Console.ReadLine();
+            responce = Convert.ToInt32(stringResponce);
+
+            if (responce != 0)
+            {
+                numbers.Add(responce);
+            }
+        }
+
+        int sum = numbers.Sum();
+        int count = numbers.Count();
+        int average = sum / count;
+        int max = numbers.Max();
+
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {average}");
+        Console.WriteLine($"The largesnt number is: {max}");
+        
     }
 }

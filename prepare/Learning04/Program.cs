1463ea656e584f0b76e2f192a156e86771713e02
whitingcal <whitingcal01@byui.edu>
2023-06-05 20:15:37 -0600

    Did learning assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Calan Whiting", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Haley Newman", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetMathInfo());
+
+        WritingAssignment assignment3 = new WritingAssignment("Zach Larson", "European History", "The Causes of World War II");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInfo());
     }
 }

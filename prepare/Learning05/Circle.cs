ec3424b77590d0bb2b24d3bd0fa639634440ff78
whitingcal <whitingcal01@byui.edu>
2023-06-21 17:23:06 -0600

    Did Learn05

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,18 @@
+
+namespace Learn05
+{
+    public class Circle : Shape
+    {
+        private double _radius;
+
+        public Circle(string color, double radius) : base (color)
+        {
+            _radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return _radius * _radius * Math.PI;
+        }
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,30 @@
 using System;
 
-class Program
+namespace Learn05
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning05 World!");
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new List<Shape>();
+
+            Square shape1 = new Square("Red", 3);
+            shapes.Add(shape1);
+
+            Rectangle shape2 = new Rectangle("Blue", 4, 5);
+            shapes.Add(shape2);
+
+            Circle shape3 = new Circle("Green", 6);
+            shapes.Add(shape3);
+
+            foreach (Shape shape in shapes)
+            {
+                string color = shape.GetColor();
+
+                double area = shape.GetArea();
+
+                Console.WriteLine($"The {color} shape has an area of {area}.");
+            }
+        }
     }
 }
